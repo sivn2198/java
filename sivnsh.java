f441c5677887cd151893acbe23db3ae56bad3878
sivn2198 <43056845+sivn2198@users.noreply.github.com>
2018-09-07 11:55:58 +0530

    Add files via upload

---
+++ sivnsh.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a=1;b=2;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("added value:"+c);
+    System.out.println("subtracted value:"+d);
+    System.out.println("mulptiplied value:"+e);
+    System.out.println("division value:"+f);
+    }
+}
