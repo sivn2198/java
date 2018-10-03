a6013c62685467dae80b1269a67d54680aa4538a
sivn2198 <43056845+sivn2198@users.noreply.github.com>
2018-10-03 12:24:18 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    int number=3;
+    if(number > 0)
+    { 
+      System.out.println(number+" is a positive number");
+    }
+    else if(number<0)
+    {
+      System.out.println(number+" number is  negative");
+    }
+    else
+    { 
+      System.out.println(number+"is neither positive nor negative");
+    }
+    
+  }
+}
