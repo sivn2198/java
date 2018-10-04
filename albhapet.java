5a38dd22678de5e210cdda4a2401cddc25e506b4
sivn2198 <43056845+sivn2198@users.noreply.github.com>
2018-10-04 12:12:53 +0530

    Add files via upload

---
+++ albhapet.java
@@ -0,0 +1,12 @@
+class Main  {
+
+    public static void main(String[] args) {
+
+        char c = 'a';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
