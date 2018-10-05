15187054d24f840361eadfc047874b86f35244c9
sivn2198 <43056845+sivn2198@users.noreply.github.com>
2018-10-05 12:11:39 +0530

    Add files via upload

---
+++ numberofdigits.java
@@ -0,0 +1,16 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 5112;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
